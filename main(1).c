ca92ce2235cf6f7d2cdb2bdeb6ce86fb1f09eba9
fontray <100141401+fontray@users.noreply.github.com>
2022-05-16 17:21:20 +0800

    Add files via upload

---
+++ main(1).c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+basic part
+struct name{
+    string first_name;
+    string middle_name;
+    string extra_name;
+};
+
+struct item{
+        int AST;//assist
+        int BLK;//block
+        int STL;//steal
+        int REB;//rebound(board)
+        float FG;//field goal
+        char POS;//position
+        int PTS;//total point
+        struct name total_name;//total name
+        string team;
+        struct item *next;
+};
+
+void Add();/*Add a new item to the database system. The program must print an error message if
+the item is already in the database.*/
+
+void Delete();/*Delete a given item from the database. The program must print an error message if
+there is no matching item to be deleted in the database.*/
+
+void Traverse();/*Print all items in the database in a specific format.*/
+
+void Search();/*Given certain information about the item, the program needs to find and print the
+specific item*/
+
+void Sort(struct item *head,int data,int dir);/*Sort all the items in ascending or descending order according to the given data. For
+example, you can sort products by their prices or expired dates*/
+
+int main()
+{
+        //File I/O
+
+        //function
+        Add total_name team data;
+        Delete name;
+        Search name;
+        Search team;
+        sort data sequence;
+
+        return 0;
+}
+
+void Add(){
+
+}
+
+void Delete(){
+
+}
+
+void Traverse(){
+
+}
+
+void Search(){
+
+}
+
+void Sort(struct item *head,int data,int dir){
+        int swapped; 
+        struct item *p; 
+        struct item *lastptr = NULL; 
+  
+        // Check for empty list    
+        do{ 
+                swapped = 0; 
+                p = head; 
+                while (p->next != lastptr) { //check if the last second node
+                        if (p->data > p->next->data){  
+                        swap(p, p->next); 
+                        swapped = 1; 
+                        } 
+                        p = p->next; //move to next node
+                } 
+                lastptr = p; //update the last node
+        } 
+        while (swapped);//check if swap happened
+}
