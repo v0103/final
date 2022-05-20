cbfc705b752276575a118b4f7d669ff5070b4f11
fontray <100141401+fontray@users.noreply.github.com>
2022-05-20 11:12:02 +0800

    Update function.c

--- function.c
+++ function.c
@@ -96,3 +96,77 @@ void Search(struct item **HEAD, char *type, char *goal)
     }
 
 }
+
+void Sort(struct item *head,int data,int dir)
+{
+    int swapped=0;
+    struct item *pre=NULL;
+    struct item *now;
+    struct item *now_next;
+    struct item *lastptr = NULL;
+    struct item *temp;
+    if (head == NULL) //check if the list is empty
+    {
+        return;
+    }
+    do
+    { 
+        swapped = 0;
+        pre = NULL;
+        now = head;
+        now_next = now->next;
+        while (now_next != lastptr) //check if the non swapped last node
+        { 
+            if(strcmp(dir,"descending")==0)  //sort with descending
+            {
+                if (now->data < now_next->data)
+                {  
+                    Swap(pre, now, now_next); 
+                    swapped = 1; //swap happened
+                    temp = now; //change swapped node pointer
+                    now = now_next;
+                    now_next = temp;
+                    if(pre==NULL)  //change head point to new one
+                    { 
+                        head = p;
+                    } 
+                }
+            }
+            else if(strcmp(dir,"ascending")==0) //sort with ascending
+            {
+                if (now->data > now_next->data)
+                {  
+                    Swap(pre, now, now_next); 
+                    swapped = 1; //swap happened
+                    temp = now; //change swapped node pointer
+                    now = now_next;
+                    now_next = temp;
+                    if(pre==NULL)  //change head point to new one
+                    { 
+                        head = p;
+                    } 
+                }
+            }
+            pre = now;  //move to next node
+            now = now_next; 
+            now_next = now_next->next;
+         } 
+         lastptr = now_next; //update the last node
+     } 
+     while (swapped);  //check if swap happened
+}
+
+void Swap(struct item *previous,struct item *a,struct item *b) 
+{
+    if(previous == NULL) //which we want to swap have head node
+    {
+        a->next = b->next;
+        b->next = a;
+    }
+    else
+    {
+        previous->next = b;
+        a->next = b->next;
+        b->next = a;
+    }
+}
