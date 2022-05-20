207f36371d9423b0c6076ae163931938eb23016e
Hsuanyu-wang <91458064+Hsuanyu-wang@users.noreply.github.com>
2022-05-20 10:27:56 +0800

    Update main(1).c

--- main(1).c
+++ main(1).c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Search.c"
-#include "Traverse.c"
+#include "basic.h" 
+#include "Search.h"
+#include "Traverse.h"
+#include "Sort.h"
 
 //basic.h
 basic part
