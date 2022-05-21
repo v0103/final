acd2cf7efdc031c86f7f35a42314fc1e87fc65a6
GaryHuang30279 <100335890+GaryHuang30279@users.noreply.github.com>
2022-05-21 12:36:14 +0800

    Add files via upload

---
+++ Delete.c
@@ -0,0 +1,27 @@
+void Delete(struct item *, struct item *);
+
+void Delete(struct item *head, struct item *new) {
+    struct item *origin;
+    origin = head;       // record the original place
+    if (head == NULL) {  // check if the list is empty
+        printf("the list is empty\n");
+        return;
+    }
+    for (head=origin; head != NULL; head = head->next) {          //if the item is already in the database.
+        if (strcmp(new->total_name, head->total_name)== 0) {      
+           struct item *temp;
+			if(head->next->next!=NULL){   //delete the middle one
+				temp=head->next->next;
+				free(head->next);
+				head->next=temp;
+			}else{                        //delete the last one
+				free(head->next);
+				head->next=NULL;
+			}
+			head=origin;
+			return;
+        }
+    }
+    printf("there is no matching item to be deleted in the database.\n");
+
+}
