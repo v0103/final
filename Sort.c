bee0c112b1b9981df2d1fb35dbd2c026d5253aad
fontray <100141401+fontray@users.noreply.github.com>
2022-05-30 00:35:56 +0800

    Update Sort.c

--- Sort.c
+++ Sort.c
@@ -386,3 +386,16 @@ void Sort_descending(struct item *head,char data[10])
      }
      while (swapped);  //check if swap happened
 }
+
+void Swap(struct item *previous,struct item *a,struct item *b)
+{
+    if(previous == NULL) {  //which we want to swap include head node
+        a->next = b->next;
+        b->next = a;
+    }
+    else {
+        previous->next = b;
+        a->next = b->next;
+        b->next = a;
+    }
+}
