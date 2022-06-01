c7540c8325c818fd9f022159279077ecf91ffae5
v0103 <73995700+v0103@users.noreply.github.com>
2022-06-02 01:06:17 +0800

    Update Delete.c

--- Delete.c
+++ Delete.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "basic.h"
-#include "Delete.h"
+void Delete(struct item *, char *Name);
 
-void Delete(struct item *head, struct item *target)
-{
+void Delete(struct item *head, char *Name) {
     /*
     example:
-    Delete(list,target);
+    Delete(list,"target_name");
     */
     struct item *origin;
+    char Team[32];
     origin = head;       // record the original place
     if (head == NULL) {  // check if the list is empty
         printf("the list is empty.\n");
         return;
     }
+    Search(&list, "NAME", Name);      //Firstly print the same names in whole teams
+    scanf("%s\n", Team);                     //Then enter  the target team
+    if (strcmp(Name, origin->total_name)== 0 && strcmp(Team, origin->team)== 0) {   //delete the first one
+	head=head->next;
+	return;
+    }
     for (head=origin; head != NULL; head = head->next) {          //if the item is already in the database.
-        if (strcmp(target->total_name, head->total_name)== 0) {
-           struct item *temp;
-	   if(head->next->next!=NULL){   //delete the middle one
+        if (strcmp(Name, head->next->total_name)== 0 && strcmp(Team, head->next->team)== 0) {  //stop at the previous one & target is next one.       
+            struct item *temp;
+	    if (head->next->next!=NULL){   //delete the middle one
 		temp=head->next->next;
 		free(head->next);
 		head->next=temp;
@@ -32,4 +34,5 @@ void Delete(struct item *head, struct item *target)
         }
     }
     printf("there is no matching item to be deleted in the database.\n");
+
 }
