42ddad384fa8ac33c53cd238ed965ef90a71339c
jush0147 <91253991+jush0147@users.noreply.github.com>
2022-06-05 01:23:50 +0800

    ast test

--- Sort.c
+++ Sort.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "basic.h"
 #include "Sort.h"
-
+/*
 void Sort_ascending(struct item *head,char data[10])
 {
     int swapped = 0;
@@ -398,4 +398,127 @@ void Swap(struct item *previous,struct item *a,struct item *b)
         a->next = b->next;
         b->next = a;
     }
+}
+*/
+
+void Sort_ascending(struct item **HEAD, char data[10]) {
+    /*
+    先取list的第一項開始往後比，遇到比自己小的就交換，直到list結束。
+    再取第二項往後比，以此類推，取到list的前一項為止。
+    (從n個裡找出最小值，再從n-1裡找最小值，......)
+    */
+
+    struct item *head = *HEAD, *cur = *HEAD, *cur_pre = NULL, *cmp = NULL, *cmp_pre = NULL;
+	int num = 0;
+    
+	while (head != NULL) {
+		num++;
+		head = head->next;
+	}
+	
+	for (int i = 0; i < num - 1; i++) {
+
+        cmp = cur->next;
+        cmp_pre = cur;
+
+        while (cmp != NULL) {
+
+            if (cmp->AST < cur->AST) {
+                
+                //swap
+                struct item *temp;
+
+                if (cur_pre != NULL) {
+			        cur_pre->next = cmp;
+		        } else {
+			        *HEAD = cmp;
+		        }
+
+		        if (cmp_pre != NULL) {
+			        cmp_pre->next = cur;
+		        } else {
+			        *HEAD = cur;
+		        }
+
+		        temp = cur->next;
+		        cur->next = cmp->next;
+		        cmp->next = temp;
+
+                //指回原本位置
+                temp = cur;
+                cur = cmp;
+                cmp = temp;
+
+            }
+
+            cmp_pre = cmp;
+            cmp = cmp->next;
+        }
+
+        cur_pre = cur;
+        cur = cur->next;
+		
+    }
+
+}
+
+void Sort_descending(struct item **HEAD, char data[10]) {
+    /*
+    先取list的第一項開始往後比，遇到比自己小的就交換，直到list結束。
+    再取第二項往後比，以此類推，取到list的前一項為止。
+    (從n個裡找出最小值，再從n-1裡找最小值，......)
+    */
+
+    struct item *head = *HEAD, *cur = *HEAD, *cur_pre = NULL, *cmp = NULL, *cmp_pre = NULL;
+	int num = 0;
+    
+	while (head != NULL) {
+		num++;
+		head = head->next;
+	}
+	
+	for (int i = 0; i < num - 1; i++) {
+
+        cmp = cur->next;
+        cmp_pre = cur;
+
+        while (cmp != NULL) {
+
+            if (cmp->AST > cur->AST) {
+                
+                //swap
+                struct item *temp;
+
+                if (cur_pre != NULL) {
+			        cur_pre->next = cmp;
+		        } else {
+			        *HEAD = cmp;
+		        }
+
+		        if (cmp_pre != NULL) {
+			        cmp_pre->next = cur;
+		        } else {
+			        *HEAD = cur;
+		        }
+
+		        temp = cur->next;
+		        cur->next = cmp->next;
+		        cmp->next = temp;
+
+                //指回原本位置
+                temp = cur;
+                cur = cmp;
+                cmp = temp;
+
+            }
+
+            cmp_pre = cmp;
+            cmp = cmp->next;
+        }
+
+        cur_pre = cur;
+        cur = cur->next;
+		
+    }
+
 }
