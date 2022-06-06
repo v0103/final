8e6d4e29431877969385e83f09e94b346a697e86
fontray <100141401+fontray@users.noreply.github.com>
2022-06-06 11:47:55 +0800

    Update and rename best_player.h to bestplayer.h

---
+++ bestplayer.h
@@ -0,0 +1,7 @@
+#ifndef BESTPLAYER_H
+#define BESTPLAYER_H
+  /************************************************************************
+   * Traverse: Print all items in the database in a specific format.      *
+   ************************************************************************/
+  void best_player();
+#endif
