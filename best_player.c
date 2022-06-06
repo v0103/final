0b1a11588c1eee364ac60aaa7e04c661432f9655
fontray <100141401+fontray@users.noreply.github.com>
2022-06-06 11:45:25 +0800

    Update best_player.c

--- best_player.c
+++ best_player.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "basic.h"
+#include "best_player.h"
 
+void best_player(struct item *head)
+{
+  struct item  *p= head;
+  struct item *temp = NULL;
+  while(p!=NULL){
+    if(p->PTS < p->next->PTS){
+      temp = p->next;
+    }
+    p = p->next;
+  }
+  printf("SEASON POINT Leader %s %s\n",temp->total_name,temp->team);
+}
