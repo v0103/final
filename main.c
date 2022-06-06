c18e42d71f667dc920ea1183ee348f5ec5b8ba2b
fontray <100141401+fontray@users.noreply.github.com>
2022-06-06 12:43:32 +0800

    Update main.c

--- main.c
+++ main.c
@@ -161,7 +161,7 @@ int main()
         else if(option == 6)
         {
             char cmp_data[10];
-            printf("input AST/BLK/STL/REB/FG:");
+            printf("input AST/BLK/STL/REB/FG/PTS:");
             scanf("%s", cmp_data);
             Sort_descending(list, cmp_data);//too long?
             //Traverse(&list);
