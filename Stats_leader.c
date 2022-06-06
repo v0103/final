b4f89d836b4a3dd0f95808e9a8d0a2cee49662f5
jush0147 <91253991+jush0147@users.noreply.github.com>
2022-06-06 12:21:47 +0800

    Add files via upload

---
+++ Stats_leader.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "basic.h"
+#include "Stats_leader.h"
+#include "Sort.h"
+
+void print_PTS(struct item **);
+void print_REB(struct item **);
+void print_AST(struct item **);
+void print_STL(struct item **);
+void print_BLK(struct item **);
+void print_FG(struct item **);
+
+void Stats_leader(struct item **HEAD) {
+    print_PTS(HEAD);
+    print_REB(HEAD);
+    print_AST(HEAD);
+    print_STL(HEAD);
+    print_BLK(HEAD);
+    print_FG(HEAD);
+}
+
+void print_PTS(struct item **HEAD) {
+
+    printf("\nPTS leader\nNAME                 \t\tPTS\n");
+    Sort_descending(HEAD, "PTS");
+
+    struct item *temp = *HEAD;
+    for (int i = 0; i < 5; i++) {
+
+        printf("%-25s\t%d\n", temp->total_name, temp->PTS);
+
+        temp = temp->next;
+    }
+}
+
+void print_REB(struct item **HEAD) {
+
+    printf("\nREB leader\nNAME                 \t\tREB\n");
+    Sort_descending(HEAD, "REB");
+
+    struct item *temp = *HEAD;
+    for (int i = 0; i < 5; i++) {
+
+        printf("%-25s\t%d\n", temp->total_name, temp->REB);
+
+        temp = temp->next;
+    }
+}
+
+void print_AST(struct item **HEAD) {
+
+    printf("\nAST leader\nNAME                 \t\tAST\n");
+    Sort_descending(HEAD, "AST");
+
+    struct item *temp = *HEAD;
+    for (int i = 0; i < 5; i++) {
+
+        printf("%-25s\t%d\n", temp->total_name, temp->AST);
+
+        temp = temp->next;
+    }
+}
+
+void print_STL(struct item **HEAD) {
+
+    printf("\nSTL leader\nNAME                 \t\tSTL\n");
+    Sort_descending(HEAD, "STL");
+
+    struct item *temp = *HEAD;
+    for (int i = 0; i < 5; i++) {
+
+        printf("%-25s\t%d\n", temp->total_name, temp->STL);
+
+        temp = temp->next;
+    }
+}
+
+void print_BLK(struct item **HEAD) {
+
+    printf("\nBLK leader\nNAME                 \t\tBLK\n");
+    Sort_descending(HEAD, "BLK");
+
+    struct item *temp = *HEAD;
+    for (int i = 0; i < 5; i++) {
+
+        printf("%-25s\t%d\n", temp->total_name, temp->BLK);
+
+        temp = temp->next;
+    }
+}
+
+void print_FG(struct item **HEAD) {
+
+    printf("\nFG%% leader\nNAME                 \t\tFG%%\n");
+    Sort_descending(HEAD, "FG%%");
+
+    struct item *temp = *HEAD;
+    for (int i = 0; i < 5; i++) {
+
+        printf("%-25s\t%.2f\n", temp->total_name, temp->FG);
+
+        temp = temp->next;
+    }
+}
---
+++ Stats_leader.h
@@ -0,0 +1,7 @@
+#ifndef STATS_LEADER_H
+#define STATS_LEADER_H
+  /************************************************************************
+   * Traverse: Print top 5 player of each stat.      *
+   ************************************************************************/
+  void Stats_leader(struct item **HEAD);
+#endif
