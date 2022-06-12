a8346c78360ba56d534cb1390fa7783910f7dd57
v0103 <73995700+v0103@users.noreply.github.com>
2022-06-12 10:07:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main()
 
     /*file in*/
     FILE *fp;
-    fp = fopen(FILE_NAME, "r+"); //open file ,r,w,a which one?
+    fp = fopen(FILE_NAME, "r+"); 
     if (fp == NULL)
     {
         printf("Can't open %s\n", FILE_NAME);
