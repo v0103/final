a451a77a8578d0cfac7db7e6983ac152a2fe7099
fontray <100141401+fontray@users.noreply.github.com>
2022-06-12 10:14:03 +0800

    Update Stats_leader.c

--- Stats_leader.c
+++ Stats_leader.c
@@ -19,6 +19,7 @@ void Stats_leader(struct item **HEAD) {
     print_STL(HEAD);
     print_BLK(HEAD);
     print_FG(HEAD);
+    
 }
 
 void print_PTS(struct item **HEAD) {
@@ -103,4 +104,110 @@ void print_FG(struct item **HEAD) {
 
         temp = temp->next;
     }
+}
+
+void best_player(struct item *head)
+{
+  
+  struct item *p=head;
+  int max;
+  int val = 0;
+  
+  max = p->PTS;
+  while(p!=NULL){
+    	if(max < p->PTS){
+      		max = p->PTS;
+    	}
+    	p = p->next;
+  }
+  p = head;
+  while(p!=NULL && val != 1){
+  	if(p->PTS != max){
+  		p = p->next;
+  	}
+  	else{
+  		val = 1;
+  	}
+  }
+  printf("SEASON POINT Leader: %s (%s) %d\n",p->total_name,p->team,p->PTS);
+  
+  p = head;
+  val = 0;
+  max = p->BLK;
+  while(p!=NULL){
+    	if(max < p->BLK){
+      		max = p->BLK;
+    	}
+    	p = p->next;
+  }
+  p = head;
+  while(p!=NULL && val != 1){
+  	if(p->BLK != max){
+  		p = p->next;
+  	}
+  	else{
+  		val = 1;
+  	}
+  }
+  printf("SEASON BLOCK Leader: %s (%s) %d\n",p->total_name,p->team,p->BLK);
+  
+  p = head;
+  val = 0;
+  max = p->AST;
+  while(p!=NULL){
+    	if(max < p->AST){
+      		max = p->AST;
+    	}
+    	p = p->next;
+  }
+  p = head;
+  while(p!=NULL && val != 1){
+  	if(p->AST != max){
+  		p = p->next;
+  	}
+  	else{
+  		val = 1;
+  	}
+  }
+  printf("SEASON ASSIST Leader: %s (%s) %d\n",p->total_name,p->team,p->AST);
+  
+  p = head;
+  val = 0;
+  max = p->STL;
+  while(p!=NULL){
+    	if(max < p->STL){
+      		max = p->STL;
+    	}
+    	p = p->next;
+  }
+  p = head;
+  while(p!=NULL && val != 1){
+  	if(p->STL != max){
+  		p = p->next;
+  	}
+  	else{
+  		val = 1;
+  	}
+  }
+  printf("SEASON STEAL Leader: %s (%s) %d\n",p->total_name,p->team,p->STL);
+  
+  p = head;
+  val = 0;
+  max = p->REB;
+  while(p!=NULL){
+    	if(max < p->REB){
+      		max = p->REB;
+    	}
+    	p = p->next;
+  }
+  p = head;
+  while(p!=NULL && val != 1){
+  	if(p->REB != max){
+  		p = p->next;
+  	}
+  	else{
+  		val = 1;
+  	}
+  }
+  printf("SEASON REBOUND Leader: %s (%s) %d\n",p->total_name,p->team,p->REB);
 }
