d9c40f2c3ba2e173320c2a77830b2247224cc139
GaryHuang30279 <100335890+GaryHuang30279@users.noreply.github.com>
2022-06-12 14:01:50 +0800

    Update Delete.c

--- Delete.c
+++ Delete.c
@@ -5,24 +5,27 @@
 #include "Delete.h"
 #include "Search.h"
 
-void Delete(struct item *head, char *Name) {
+struct item *Delete(struct item *head, char *Name) {
     /*
     example:
-    Delete(list,"target_name");
+    list=Delete(list,"target_name");
     */
     struct item *origin;
     char Team[32];
     origin = head;       // record the original place
     if (head == NULL) {  // check if the list is empty
         printf("the list is empty.\n");
-        return;
+        return origin;
     }
     Search(&head, "NAME", Name);      //Firstly print the same names in whole teams
     printf("which team :");
     scanf("%s", Team);                     //Then enter  the target team
     if (strcmp(Name, origin->total_name)== 0 && strcmp(Team, origin->team)== 0) {   //delete the first one
-	head=head->next;
-	return;
+	struct item *temp;
+        temp = head->next;
+        free(head);
+        head = head->next;
+        return head;
     }
     for (head=origin; head != NULL; head = head->next) {          //if the item is already in the database.
         if (strcmp(Name, head->next->total_name)== 0 && strcmp(Team, head->next->team)== 0) {  //stop at the previous one & target is next one.
@@ -31,14 +34,13 @@ void Delete(struct item *head, char *Name) {
 		temp=head->next->next;
 		free(head->next);
 		head->next=temp;
-	   }else{                        //delete the last one
+	    }else{                        //delete the last one
 		free(head->next);
 		head->next=NULL;
-	   }
-	   head=origin;
-	   return;
+	    }
+	    return origin;
         }
     }
     printf("there is no matching item to be deleted in the database.\n");
-
+    return origin;
 }
