58d6eff8ac8113c33d426cc0fc650538922c5ce6
fontray <100141401+fontray@users.noreply.github.com>
2022-06-12 11:26:52 +0800

    Update Sort.c

--- Sort.c
+++ Sort.c
@@ -248,9 +248,9 @@ void Sort_ascending(struct item **HEAD, char data[10]) {
 
 void Sort_descending(struct item **HEAD, char data[10]) {
     /*
-    先取list的第一項開始往後比，遇到比自己小的就交換，直到list結束。
+    先取list的第一項開始往後比，遇到比自己大的就交換，直到list結束。
     再取第二項往後比，以此類推，取到list的前一項為止。
-    (從n個裡找出最小值，再從n-1裡找最小值，......)
+    (從n個裡找出最大值，再從n-1裡找最大值，......)
     */
 
     struct item *head = *HEAD, *cur = *HEAD, *cur_pre = NULL, *cmp = NULL, *cmp_pre = NULL;
