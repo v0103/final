bde2ede8f7f398bc02afd9bf7d1be07142902f3c
v0103 <73995700+v0103@users.noreply.github.com>
2022-06-12 10:41:51 +0800

    Update FILEIO.h

--- FILEIO.h
+++ FILEIO.h
@@ -4,6 +4,7 @@
    * Traverse: Print all items in the database in a specific format.      *
    ************************************************************************/
   //void _READ();
-  struct item* _BUILD(FILE *fp);
+  struct item* _BUILD();
+  void _update_file();
   //void _print(struct item *list);
 #endif
